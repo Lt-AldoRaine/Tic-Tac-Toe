e05f4894c4c59ac5c8db1fed4d153e6d01d93446
Aldo Raine <105580292+Lt-AldoRaine@users.noreply.github.com>
2023-03-21 02:58:26 -0500

    Add hover effect for current player sign on cursor position

--- main.js
+++ main.js
@@ -27,8 +27,8 @@ const Player = (sign, turn) => {
 const GameController = (() => {
   const playerOne = Player("X", true);
   const playerTwo = Player("O", false);
-  const board = GameBoard.getBoard();
   let turn = 1;
+  let gameOver = false;
 
   const getCurrentPlayer = () => {
     return turn % 2 === 1 ? playerOne.sign : playerTwo.sign;
@@ -38,38 +38,71 @@ const GameController = (() => {
     GameBoard.setBoard(index, getCurrentPlayer());
     if (turn === 9) {
       resetGame();
+      gameOver = true;
       return;
     }
     turn++;
   };
 
+  const getGameOver = () => gameOver;
+
   const resetGame = () => {
     turn = 1;
+    gameOver = false;
   };
-  return { playerStep, resetGame };
+  return { playerStep, resetGame, getCurrentPlayer, getGameOver };
 })();
 
 const DisplayController = (() => {
   const boardSquares = document.querySelectorAll(".board-square");
   const resetButton = document.querySelector(".reset-board");
 
-  boardSquares.forEach((square) => {
-    square.addEventListener("click", (e) => {
-      GameController.playerStep(parseInt(e.target.dataset.square));
-      updateBoard();
-    });
-  });
-
-  resetButton.addEventListener("click", () => {
-    GameBoard.resetBoard();
+  const reset = () => {
     GameController.resetGame();
+    GameBoard.resetBoard();
     updateBoard();
-  });
+  };
+
+  const toggleHover = (square) => {
+    square.addEventListener("mouseenter", () => {
+      if (!square.classList.contains("hover") && square.innerText === "") {
+        square.innerText = GameController.getCurrentPlayer();
+        square.classList.add("hover");
+      }
+    });
+    square.addEventListener("mouseleave", () => {
+      if (square.classList.contains("hover") && square.innerText !== "") {
+        square.innerText = "";
+        square.classList.remove("hover");
+      }
+    });
+  };
+
+  const placeSign = (square) => {
+    GameController.playerStep(parseInt(square.dataset.square));
+    if (GameController.getGameOver()) reset();
+    if (square.classList.contains("hover")) {
+      square.classList.remove("hover");
+    }
+    updateBoard();
+  };
 
   const updateBoard = () => {
     for (let i = 0; i < boardSquares.length; i++) {
       boardSquares[i].innerText = GameBoard.getBoard(i);
     }
   };
+
+  boardSquares.forEach((square) => {
+    square.addEventListener("click", (e) => {
+      if (square.classList.contains("hover")) {
+        placeSign(square);
+      }
+    });
+    toggleHover(square);
+  });
+
+  resetButton.addEventListener("click", () => {
+    reset();
+  });
 })();
-console.log("joe");
